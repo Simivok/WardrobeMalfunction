09efa1f1e231f8d4ab620684e1a6bba44f3541c3
Simivok <bence8035@gmail.com>
2025-04-20 22:38:06 +0200

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Brand;
+
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+
+class AdminController extends Controller
+{
+    public function index()
+    {     
+        return view('admin.index');
+    
+    }
+    public function brands(){
+
+        $brands = Brand::orderBy('id','DESC')->paginate(10);
+        return view('admin.brands',compact('brands'));
+    }
+
+    public function add_brand()  {
+        return view('admin.brand-add');
+    }
+
+    public function brand_store(Request $request)  {
+        $request->validate([
+            'name'=> 'required',
+            'slug'=> 'required|unique:brands,slug',
+            'image'=> 'required|mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->file('image')) {
+			$filePath = $request->file('image')->store('image', 'public');
+		}
+        $brand->image = $filePath;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status','Brand has been added successfully!'); 
+    }
+
+    public function brand_edit($id){
+        $brand = Brand::find($id);
+        return view('admin.brand-edit',compact('brand'));
+    }
+
+    public function brand_update(Request $request){
+        $request->validate([
+            'name'=> 'required',
+            'slug'=> 'required|unique:brands,slug'.$request->id,
+            'image'=> 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if($request->hasFile('image')){
+            if(File::exists(public_path('uploads/brands').'/'.$brand->image)){
+                File::delete(public_path('uploads/brands').'/'.$brand->image);
+            }
+            if ($request->file('image')) {
+                $filePath = $request->file('image')->store('image', 'public');
+            }
+            $brand->image = $filePath;
+           
+        };
+        
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status','Brand has been updated successfully!');
+    }
+
+    
+    public function brand_delete($id){
+        $brand = Brand::find($id);
+        if (File::exists(public_path('uploads/brands').'/'.$brand->image)) {
+            File::delete(public_path('uploads/brands').'/'.$brand->image);
+        }
+        $brand->delete();
+        return redirect()->route('admin.brands')->with('status','Brand has been deleted successfully');
+    }
+    public function categories(){
+        $categories = Category::orderBy('id','DESC')->paginate(10);
+        return view('admin.categories',compact('categories'));
+    }
+    public function category_add(){
+        return view('admin.category-add');
+    }
+    public function category_store(Request $request) 
+    {
+        $request->validate([
+            'name'=> 'required',
+            'slug'=> 'required|unique:categories,slug',
+            'image'=> 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        if ($request->file('image')) {
+			$filePath = $request->file('image')->store('image', 'public');
+		}
+        $category->image = $filePath;
+        $category->save();
+        return redirect()->route('admin.categories')->with('status','Category has been added successfully!'); 
+    }
+    
+}
---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\AuthAdmin;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Models\Brand;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/account-dashboard',[UserController::class,'index'])->name('user.index');
+    
+});
+
+Route::middleware(['auth',AuthAdmin::class])->group(function() {
+    Route::get('/admin',[AdminController::class,'index'])->name('admin.index');
+    Route::get('/admin/brands',[AdminController::class,'brands'])->name('admin.brands');
+    Route::get('/admin/brand/add',[AdminController::class,'add_brand'])->name('admin.brand.add');
+    Route::post('/admin/brand/store',[AdminController::class,'brand_store'])->name('admin.brand.store');
+    Route::get('/admin/brand/edit/{id}',[AdminController::class,'brand_edit'])->name('admin.brand.edit');
+    Route::put('/admin/brand/update',[AdminController::class,'brand_update'])->name('admin.brand.update');
+    Route::delete('/admin/brand/{id}/delete',[AdminController::class,'brand_delete'])->name('admin.brand.delete');
+    Route::get('/admin/categories',[AdminController::class, 'categories'])->name('admin.categories');
+    Route::get('/admin/category/add',[AdminController::class,'category_add'])->name('admin.category.add');
+    Route::post('/admin/category/store',[AdminController::class,'category_store'])->name('admin.category.store');
+});
