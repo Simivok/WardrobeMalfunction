4214310714ef4bac5fbdd023e2f4372150f3e243
Simivok <bence8035@gmail.com>
2025-04-22 19:06:49 +0200

    Product edit

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 use App\Models\Brand;
-
 use App\Models\Category;
+use App\Models\Product;
+
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
@@ -144,6 +145,117 @@ class AdminController extends Controller
         $category->delete();
         return redirect()->route('admin.categories')->with('status','Category has been deleted successfully');
     }
+
+    public function products()
+    {
+        $products = Product::orderBy('created_at','DESC')->paginate(10);
+        return view('admin.products',compact('products'));
+    }
+    public function product_add()
+    {
+        $categories = Category::select('id','name')->orderBy('name')->get();
+        $brands = Brand::select('id','name')->orderBy('name')->get();
+        return view('admin.product-add', compact('categories','brands'));
+    }
+
+    public function product_store(Request $request)
+    {
+        $request->validate([
+       
+            'name' => 'required',
+            'slug' => 'required|unique:products,slug',
+            'short_description' => 'required',
+            'description' => 'required',
+            'regular_price' => 'required',
+            'sale_price' => 'required',
+            'SKU' => 'required',
+            'stock_status' => 'required',
+            'featured' => 'required',
+            'quantity' => 'required',
+            'image' => 'required|mimes:png,jpg,jpeg|max:2048',
+            'category_id' => 'required',
+            'brand_id' => 'required'
+        ]);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $current_timestamp = Carbon::now()->timestamp;
+        if ($request->file('image')) {
+            $filePath = $request->file('image')->store('image', 'public');
+            $product->image = $filePath;
+        }
+       
+       
+        
+
+        $product->save();
+        return redirect()->route('admin.products')->with('status','Product has been added successfully');
+    }
     
-    
+    public function product_edit($id)
+{
+    $product = Product::find($id);
+    $categories = Category::select('id','name')->orderBy('name')->get();
+    $brands = Brand::select('id','name')->orderBy('name')->get();
+    return view('admin.product-edit',compact('product','categories','brands'));
+}
+
+public function product_update(Request $request)
+{
+    $request->validate([
+        
+        'name' => 'required',
+        'slug' => 'required|unique:products,slug'.$request->id,
+        'short_description' => 'required',
+        'description' => 'required',
+        'regular_price' => 'required',
+        'sale_price' => 'required',
+        'SKU' => 'required',
+        'stock_status' => 'required',
+        'featured' => 'required',
+        'quantity' => 'required',
+        'image' => 'required|mimes:png,jpg,jpeg|max:2048',
+        'category_id' => 'required',
+        'brand_id' => 'required'
+    ]);
+        $product = Product::find($request->id);
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+        $current_timestamp = Carbon::now()->timestamp;
+
+        if ($request->file('image')) {
+            if (File::exists(public_path('uploads/products').'/'.$product->image)) {
+                File::delete(public_path('uploads/products').'/'.$product->image);
+            }
+            $filePath = $request->file('image')->store('image', 'public');
+            $product->image = $filePath;
+        }
+       
+       
+        
+
+        $product->save();
+        return redirect()->route('admin.products')->with('status','Product has been updated successfully!');
+   } 
 }
--- routes/web.php
+++ routes/web.php
@@ -44,4 +44,12 @@ Route::middleware(['auth',AuthAdmin::class])->group(function() {
     Route::get('/admin/category/edit/{id}',[AdminController::class,'category_edit'])->name('admin.category.edit');
     Route::put('/admin/category/update',[AdminController::class,'category_update'])->name('admin.category.update');
     Route::delete('/admin/category/{id}/delete',[AdminController::class,'category_delete'])->name('admin.category.delete');
+
+    Route::get('/admin/products',[AdminController::class,'products'])->name('admin.products');
+    Route::get('/admin/product/add',[AdminController::class,'product_add'])->name('admin.product.add');
+    Route::post('/admin/product/store',[AdminController::class,'product_store'])->name('admin.product.store');
+    Route::get('/admin/product/edit/{id}',[AdminController::class,'product_edit'])->name('admin.product.edit');
+    Route::put('/admin/product/update',[AdminController::class,'product_update'])->name('admin.product.update');
+    
+
 });
